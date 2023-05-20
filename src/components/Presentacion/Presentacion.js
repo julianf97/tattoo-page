dc5fe3201a5320ae8eb0cad8d6828377581ead2b
Julian Finelli <julianfinelli97@gmail.com>
2023-05-20 11:10:59 -0300

    presentacion

--- src/components/Presentacion/Presentacion.js
+++ src/components/Presentacion/Presentacion.js
@@ -7,6 +7,19 @@ export const Presentacion = () => {
     return(
         <div className='contenedorGeneral'>
             <div className='contenedorTituloyBtn'>
+                <div className='contenedorInternoTituloyBtn'>
+                    <div className='contenedorTitulo'>
+                        <h1>Deft Ink</h1>
+                        <div className='contenedorSubTitulo'>
+                            San Diego Tattoo Artist
+                        </div>
+                    </div>
+                    <div className='contenedorBtn'>
+                        <button className='btn'>   
+                            MAKE ME AN APPOINTMENT NOW
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     )
