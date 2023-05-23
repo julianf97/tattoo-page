f0585aeffe94423ceee6793ff096ec83eb8dc00e
Julian Finelli <julianfinelli97@gmail.com>
2023-05-23 17:14:59 -0300

    siguiendo

--- src/components/Secciones/Secciones.js
+++ src/components/Secciones/Secciones.js
@@ -6,7 +6,17 @@ export const Secciones = () => {
 
     return(
         <div className='contenedorPrincipalSecciones'>
+            <article className='secciones'>
+                <section className='seccion seccion1'>
 
+                </section>
+                <section className='seccion seccion2'>
+
+                </section>
+                <section className='seccion seccion3'>
+
+                </section>
+            </article>
         </div>
     )
 
