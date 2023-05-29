13f2c2ee59606cb7d7b035d7d4ebff584392e710
Julian Finelli <julianfinelli97@gmail.com>
2023-05-29 16:44:24 -0300

    agregando estilos

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Presentacion } from './components/Presentacion/Presentacion';
 import { Navbar } from './components/Navbar/Navbar'
 import { Secciones } from './components/Secciones/Secciones';
 import './App.css';
+import { Descripcion } from './components/Descripcion/Descripcion';
 
 
 
@@ -12,6 +13,7 @@ function App() {
       <Navbar/>
       <Presentacion/>
       <Secciones/>
+      <Descripcion/>
     </div>
   );
 }
