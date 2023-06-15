977b9ba06adbb591e5c4da1429ee841d7b94d79f
Iván Manetta <ivan.manetta@teamwire.eu>
2023-06-15 12:15:14 +0200

    arreglos

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,150 +1,164 @@
-import React from 'react'
-import './_navbarIpad.scss'
-import './_navbarMobile.scss'
-import './_navbarDesktop.scss'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { useState } from 'react'
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
-import Hamburger from 'hamburger-react'
-import  italia  from "../../assets/img/italia.png"
-import  reinoUnido  from "../../assets/img/reinoUnido.png"
-import { useContext } from 'react'
-import { LenguajeContext } from "../../context/LenguajeContext/LenguajeContext"
-import { NavbarContext } from "../../context/NavbarContext/NavbarContext"
+import React from "react";
+import "./_navbarIpad.scss";
+import "./_navbarMobile.scss";
+import "./_navbarDesktop.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import Hamburger from "hamburger-react";
+import italia from "../../assets/img/italia.png";
+import reinoUnido from "../../assets/img/reinoUnido.png";
+import { useContext } from "react";
+import { LenguajeContext } from "../../context/LenguajeContext/LenguajeContext";
+import { NavbarContext } from "../../context/NavbarContext/NavbarContext";
 
-export const Navbar = () => {
+export const Navbar = ({ isPresentacionVisible }) => {
+  const {
+    idioma,
+    handleClickModal,
+    handleClickIngles,
+    handleClickItaliano,
+    inglesElegido,
+  } = useContext(LenguajeContext);
 
-    const { idioma, handleClickModal, handleClickIngles, handleClickItaliano, inglesElegido } = useContext(LenguajeContext)
+  const { handleClickOpen } = useContext(NavbarContext);
 
-    const { handleClickOpen } = useContext(NavbarContext)
-
-    return(
-       <div className='contenedorNavbar'>
-            <div className='contenedorInternoNavbar'>
-                <div className='contenedorBarras'>
-                <Hamburger rounded onToggle={
-                    toggled => {
-                                
-                                if (toggled) {
-                                        handleClickOpen()
-                                } else {
-                                        handleClickOpen()
-                                }
-                }} size={30} color="#AF814F" />
-                    <div className='contenedorLogoDesktop'></div>
-                </div>
-                <div className='contenedorMedio'>
-                    <div className='contenedorLogoMobile'>
-
-                    </div>
-                    <div className='contenedorNavbarDesktop'>
-                        <div className='contenedorNavbarInteriorDesktop'>
-                            <nav>
-                                <ul>
-                                    {
-                                        inglesElegido ?  <li>BLACK & GRAY</li>
-                                                    :  <li>NERO E GRIGIO</li>
-                                    }
-                                    {
-                                        inglesElegido ? <li>LETTERING</li>
-                                                    :  <li>SCRITTURA</li>
-                                    }
-                                    {
-                                        inglesElegido ? <li>COMMISSIONED ART</li>
-                                                    :  <li>COMMISSIONE ART</li>
-                                    }
-                                </ul>
-                            </nav>
-                        </div>
-                    </div>
-                </div>
-                <div className='contenedorIdioma'>
-                    <div onClick={handleClickModal} className='contenedorInternoElementosIdioma'>
-                        <div className='contenedorImg'>
-                            <div className='contenedorInternoImg'>
-                                {
-                                    inglesElegido ?  <img className='imagenReinoUnidoElegida' src={reinoUnido} alt="lenguajeImg"></img>
-                                                : <img src={italia} alt="lenguajeImg"></img>
-                                }
-                            </div>
-                        </div>
-                        {
-                            inglesElegido ? <span className='btnIdioma'>ENG </span>
-                                        : <span className='btnIdioma'> ITA </span> 
-                        }
-                        {
-                            idioma ? <FontAwesomeIcon className='anguloRotado' icon={faAngleRight}></FontAwesomeIcon> 
-                                : <FontAwesomeIcon className='anguloDerecho' icon={faAngleRight}></FontAwesomeIcon> 
-                        }
-                    </div>
-                </div>
+  return (
+    <div
+      className="contenedorNavbar"
+      style={{ display: isPresentacionVisible ? "none" : "flex" }}
+    >
+      <div className="contenedorInternoNavbar">
+        <div className="contenedorBarras">
+          <Hamburger
+            rounded
+            onToggle={(toggled) => {
+              if (toggled) {
+                handleClickOpen();
+              } else {
+                handleClickOpen();
+              }
+            }}
+            size={30}
+            color="#AF814F"
+          />
+          <div className="contenedorLogoDesktop"></div>
+        </div>
+        <div className="contenedorMedio">
+          <div className="contenedorLogoMobile"></div>
+          <div className="contenedorNavbarDesktop">
+            <div className="contenedorNavbarInteriorDesktop">
+              <nav>
+                <ul>
+                  {inglesElegido ? (
+                    <li>BLACK & GRAY</li>
+                  ) : (
+                    <li>NERO E GRIGIO</li>
+                  )}
+                  {inglesElegido ? <li>LETTERING</li> : <li>SCRITTURA</li>}
+                  {inglesElegido ? (
+                    <li>COMMISSIONED ART</li>
+                  ) : (
+                    <li>COMMISSIONE ART</li>
+                  )}
+                </ul>
+              </nav>
             </div>
-            <div className='contenedorAbsoluto'>
-                <div className='contenedorInteriorAbsoluto'>
-                    <div className='contenedorBarras'>
-                        <FontAwesomeIcon className='barras' icon={faBars}></FontAwesomeIcon>
-                        <div className='contenedorLogoDesktop'></div>
-                    </div>
-                    <div className='contenedorMedio'>
-                        <div className='contenedorLogoMobile'>
-
-                        </div>
-                        <div className='contenedorNavbarDesktop'>
-                            <div className='contenedorNavbarInteriorDesktop'>
-                                <nav>
-                                    <ul>
-                                    {
-                                        inglesElegido ?  <li>BLACK & GRAY</li>
-                                        :   <li>NERO E GRIGIO</li>
-                                    }
-                                    {
-                                        inglesElegido ? <li>LETTERING</li>
-                                        :  <li>SCRITTURA</li>
-                                    }
-                                    {
-                                        inglesElegido ? <li>COMMISSIONED ART</li>
-                                        :  <li>COMMISSIONE ART</li>
-                                    }
-                                    </ul>
-                                </nav>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='contenedorIdioma'>
-                    {
-                        idioma ? <div className='selectorIdioma'>
-                                    <div className='contenedorInternoIdiomas'>
-                                        <div onClick={handleClickItaliano}  className='contenedorITA'>
-                                            <div className='contenedorNombre'>
-                                                ITA
-                                            </div>
-                                            <div className='contenedorBandera'>
-                                                <div className='contenedorInternoBandera'>
-                                                    <img src={italia} alt="lenguajeImg"></img>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div onClick={handleClickIngles} className='contenedorENG'>
-                                            <div className='contenedorNombre'>
-                                                ENG
-                                            </div>
-                                            <div className='contenedorBandera'>
-                                                <div className='contenedorInternoBandera'>
-                                                    <img src={reinoUnido} alt="lenguajeImg"></img>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                : <div></div>
-                    }
-                    </div>
-                </div>
+          </div>
+        </div>
+        <div className="contenedorIdioma">
+          <div
+            onClick={handleClickModal}
+            className="contenedorInternoElementosIdioma"
+          >
+            <div className="contenedorImg">
+              <div className="contenedorInternoImg">
+                {inglesElegido ? (
+                  <img
+                    className="imagenReinoUnidoElegida"
+                    src={reinoUnido}
+                    alt="lenguajeImg"
+                  ></img>
+                ) : (
+                  <img src={italia} alt="lenguajeImg"></img>
+                )}
+              </div>
             </div>
-       </div>
-    )
-
-}
-
-
+            {inglesElegido ? (
+              <span className="btnIdioma">ENG </span>
+            ) : (
+              <span className="btnIdioma"> ITA </span>
+            )}
+            {idioma ? (
+              <FontAwesomeIcon
+                className="anguloRotado"
+                icon={faAngleRight}
+              ></FontAwesomeIcon>
+            ) : (
+              <FontAwesomeIcon
+                className="anguloDerecho"
+                icon={faAngleRight}
+              ></FontAwesomeIcon>
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="contenedorAbsoluto">
+        <div className="contenedorInteriorAbsoluto">
+          <div className="contenedorBarras">
+            <FontAwesomeIcon className="barras" icon={faBars}></FontAwesomeIcon>
+            <div className="contenedorLogoDesktop"></div>
+          </div>
+          <div className="contenedorMedio">
+            <div className="contenedorLogoMobile"></div>
+            <div className="contenedorNavbarDesktop">
+              <div className="contenedorNavbarInteriorDesktop">
+                <nav>
+                  <ul>
+                    {inglesElegido ? (
+                      <li>BLACK & GRAY</li>
+                    ) : (
+                      <li>NERO E GRIGIO</li>
+                    )}
+                    {inglesElegido ? <li>LETTERING</li> : <li>SCRITTURA</li>}
+                    {inglesElegido ? (
+                      <li>COMMISSIONED ART</li>
+                    ) : (
+                      <li>COMMISSIONE ART</li>
+                    )}
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          </div>
+          <div className="contenedorIdioma">
+            {idioma ? (
+              <div className="selectorIdioma">
+                <div className="contenedorInternoIdiomas">
+                  <div onClick={handleClickItaliano} className="contenedorITA">
+                    <div className="contenedorNombre">ITA</div>
+                    <div className="contenedorBandera">
+                      <div className="contenedorInternoBandera">
+                        <img src={italia} alt="lenguajeImg"></img>
+                      </div>
+                    </div>
+                  </div>
+                  <div onClick={handleClickIngles} className="contenedorENG">
+                    <div className="contenedorNombre">ENG</div>
+                    <div className="contenedorBandera">
+                      <div className="contenedorInternoBandera">
+                        <img src={reinoUnido} alt="lenguajeImg"></img>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div></div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/Presentacion/Presentacion.js
+++ src/components/Presentacion/Presentacion.js
@@ -1,30 +1,40 @@
-import React from 'react'
-import './_presentacionMobile.scss'
-import './_presentacionIpad.scss'
-import './_presentacionDesktop.scss'
+import React, { useEffect, useState, forwardRef } from "react";
+import "./_presentacionMobile.scss";
+import "./_presentacionIpad.scss";
+import "./_presentacionDesktop.scss";
 
+const CANTIDAD_DE_PRESENTACIONES = 2;
 
-export const Presentacion = () => {
+export const Presentacion = forwardRef((props, ref) => {
+  const [presentacionIndex, setPresentacionIndex] = useState(1);
 
-    return(
-        <div className='contenedorGeneral'>
-            <div className='contenedorTituloyBtn'>
-                <div className='contenedorInternoTituloyBtn'>
-                    <div className='contenedorTitulo'>
-                        <h1>Deft Ink</h1>
-                        <div className='contenedorSubTitulo'>
-                            San Diego Tattoo Artist
-                        </div>
-                    </div>
-                    <div className='contenedorBtn'>
-                        <button className='btn'>   
-                            MAKE ME AN APPOINTMENT NOW
-                        </button>
-                    </div>
-                </div>
-            </div>
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setPresentacionIndex((presentacionIndex) =>
+        presentacionIndex === CANTIDAD_DE_PRESENTACIONES
+          ? 1
+          : presentacionIndex + 1
+      );
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div
+      ref={ref}
+      className={`contenedorGeneral presentacion${presentacionIndex}`}
+    >
+      <div className="contenedorTituloyBtn">
+        <div className="contenedorInternoTituloyBtn">
+          <div className="contenedorTitulo">
+            <h1>Deft Ink</h1>
+            <div className="contenedorSubTitulo">San Diego Tattoo Artist</div>
+          </div>
+          <div className="contenedorBtn">
+            <button className="btn">MAKE ME AN APPOINTMENT NOW</button>
+          </div>
         </div>
-    )
-
-}
-
+      </div>
+    </div>
+  );
+});
